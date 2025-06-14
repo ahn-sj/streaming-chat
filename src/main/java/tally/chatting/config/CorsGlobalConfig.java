53fb4a7616f53da20bdb370a6b4a321631193c9c
ahn-sj <jaesa5221@gmail.com>
2025-06-14 16:10:19 +0900

    Add CORS configuration: implement CorsGlobalConfig for global CORS settings in WebFlux

---
+++ src/main/java/tally/chatting/config/CorsGlobalConfig.java
@@ -0,0 +1,18 @@
+package tally.chatting.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsGlobalConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
