95f6be965552839619ecd113852eb0d7745740e0
ahn-sj <jaesa5221@gmail.com>
2025-06-19 18:13:48 +0900

    Add error handling for chat completion: implement getChatCompletionWithCatchException method in LlmWebClientService to handle exceptions and return CommonError responses

--- src/main/java/tally/chatting/service/llmclient/LlmWebClientService.java
+++ src/main/java/tally/chatting/service/llmclient/LlmWebClientService.java
@@ -2,6 +2,8 @@ package tally.chatting.service.llmclient;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import tally.chatting.exception.CommonError;
+import tally.chatting.exception.ErrorTypeException;
 import tally.chatting.model.llmclient.LlmChatRequestDto;
 import tally.chatting.model.llmclient.LlmChatResponseDto;
 import tally.chatting.model.llmclient.LlmType;
@@ -17,5 +19,24 @@ public interface LlmWebClientService {
      * @return LlmType
      */
     LlmType getLlmType();
+
+    default Mono<LlmChatResponseDto> getChatCompletionWithCatchException(LlmChatRequestDto requestDto) {
+        return getChatCompletion(requestDto)
+                .onErrorResume(exception -> {
+                    if (exception instanceof ErrorTypeException errorTypeException) {
+                        final CommonError commonError = new CommonError(
+                                errorTypeException.getErrorType().getCode(),
+                                errorTypeException.getMessage()
+                        );
+                        return Mono.just(new LlmChatResponseDto(commonError));
+                    } else {
+                        final CommonError commonError = new CommonError(
+                                500,
+                                exception.getMessage()
+                        );
+                        return Mono.just(new LlmChatResponseDto(commonError));
+                    }
+                });
+    }
 
 }
